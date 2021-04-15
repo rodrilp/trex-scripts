c58d6e5af770be9a93d26b2c3856fec3c46df73a
Rodrigo <rodrigolp7@gmail.com>
2021-04-15 12:46:37 +0200

    Hot fix

--- pcap.py
+++ pcap.py
@@ -5,10 +5,10 @@ c = STLClient(server = '127.0.0.1')
 c.connect()
 
 try:
-    c.reset(ports = [port_0, port_1])
+    c.reset(ports = [0, 1])
 
     # push local PCAP file 'http.pcap' over port_0 with IPG of 1 ms
-    c.push_pcap('http.pcap', ports = port_0, ipg_usec = 1000)
+    c.push_pcap('http.pcap', ports = 0, ipg_usec = 1000)
 
     # hold until traffic ends
     c.wait_on_traffic()
@@ -18,11 +18,11 @@ try:
 
     # examine stats for port_0
     print("port_0 stats:")
-    print(stats[port_0])
+    print(stats[0])
 
     # examine stats for port_1
     print("port_1 stats:")
-    print(stats[port_1])
+    print(stats[1])
 
 except STLError as e:
     print(e)
